2d149fb0ee5c719b70bd3eca8830677b6994d580
Bruno Tavares <brunobt80@gmail.com>
2024-01-23 20:16:29 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
-import database from "infra/database.js"; 
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS SUM;");
-  console.log(result.rows);
-  response.status(200).json({"status": "Olá Bruno!"});
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseResultVersionMaxConnections = await database.query(
+    "SHOW server_version; SHOW max_connections;"
+  );
+  const databaseResultOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const [databaseVersion, databaseMaxConnections] =
+    databaseResultVersionMaxConnections.map((r) => r.rows[0]);
+  const databaseOpenedConnections = databaseResultOpenedConnections.rows[0];
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(databaseMaxConnections.max_connections),
+        opened_connections: databaseOpenedConnections.count,
+        version: databaseVersion.server_version,
+      },
+    },
+  });
 }
 
 export default status;
